00318f213db10e91784ff0089d1514b336ee6aed
artLopez <arlopez@csumb.edu>
2015-03-20 19:25:03 +0000

    upgraded to JUnit to 4.12

--- src/test/java/edu/csumb/app/AppTest.java
+++ src/test/java/edu/csumb/app/AppTest.java
@@ -1,38 +1,21 @@
 package edu.csumb.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class AppTest {
+
+    @Test
+    public void thisAlwaysPasses() {
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
     }
 }
