1fe34ee8c1ee922c7ba1e21f306153c83870471b
artLopez <arlopez@csumb.edu>
2015-03-20 19:28:19 +0000

    defined integraton test

--- src/test/java/edu/csumb/app/AppTest.java
+++ src/test/java/edu/csumb/app/AppTest.java
@@ -18,4 +18,55 @@ public class AppTest {
     @Ignore
     public void thisIsIgnored() {
     }
+
+    @Test
+    public void groupsHaveThreePeople() {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        Generator generator = new App(names);
+        List<String> group = generator.createGroup("a");
+        assertEquals(group.size(), 3);
+    }
+
+    @Test
+    public void givenUsernameIsInGroup() {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        Generator generator = new App(names);
+        String name = "a";
+        List<String> group = generator.createGroup(name);
+        assertEquals(group.contains(name), true);
+    }
+
+    @Test
+    public void groupsHaveConsistentMembership() {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        Generator generator = new App(names);
+        List<String> actualA = generator.createGroup("a");
+        List<String> actualB = generator.createGroup("b");
+        List<String> actualC = generator.createGroup("c");
+        List<String> expected = new ArrayList<String>();
+        expected.add("a");
+        expected.add("b");
+        expected.add("c");
+        assertEquals(actualA, expected);
+        assertEquals(actualB, expected);
+        assertEquals(actualC, expected);
+    }
 }
